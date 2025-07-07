370e38a9f9c1cb3e756d0cfa12538a0e634e03bd
Brijes Mandal <brijes.mandal01@gmail.com>
2025-07-06 21:23:31 -0500

    Updated Local Storage Code

--- Help Request Board/script.js
+++ Help Request Board/script.js
@@ -1,41 +1,47 @@
-let requests = JSON.parse((localStorage.getItem("requests")) || []); 
-
-let submitData = [];
+let requests = JSON.parse(localStorage.getItem("requests")) || [];
+let submitData = requests;
 
 function saveData(submitData) {
     let jsonString = JSON.stringify(submitData);
-    localStorage.setItem("requests",jsonString);
+    localStorage.setItem("requests", jsonString);
 }
 
-
-document.querySelector(".form").addEventListener("submit", (e)=> {
+document.querySelector(".form").addEventListener("submit", (e) => {
     e.preventDefault();
     let name = document.getElementById("name").value.trim();
     let category = document.getElementById("category").value.trim();
     let description = document.getElementById("description").value.trim();
 
     let data = {
-        "name": name,
-        "category": category,
-        "description": description,
-    }
+        name,
+        category,
+        description
+    };
 
     submitData.push(data);
     saveData(submitData);
 
-    this.reset()
-})
+    e.target.reset(); 
+});
 
-document.getElementById("see-requests").addEventListener("click",() => {
+document.getElementById("see-requests").addEventListener("click", (e) => {
+    e.preventDefault();
     let container = document.querySelector(".requests-container");
     container.innerHTML = "";
 
-    if (requests == [])
-    {
+    if (requests.length === 0) {
         alert("No data to display!");
-        return NaN;
+        return;
     }
+
     requests.forEach(req => {
-        
+        const div = document.createElement("div");
+        div.className = "request-card";
+        div.innerHTML = `
+            <h3>${req.name} needs help with ${req.category}.</h3>
+            <p>${req.description}</p>
+        `;
+        container.appendChild(div);
     });
 });
+
